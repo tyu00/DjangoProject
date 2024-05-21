ab68d6bff894ebc6ebabb2fd803cbce3e553a724
tyu00 <g.s.u.toshov2002@yandex.com>
2024-05-21 16:42:15 +0500

    10

--- core/tests.py
+++ core/tests.py
@@ -1,18 +1,7 @@
 from django.test import TestCase
-from django.test import Client
-from core import models
 
 
 class Tests(TestCase):
-    def setUp(self):
-        self.client = Client()
-        self.user = models.User.objects.create(
-            name='Первый',
-            email='pervi@gmail.com',
-            phone='88888888888',
-            is_rare=True
-        )
-
     def test_index(self):
         response = self.client.get('/index/')
         self.assertEqual(response.status_code, 200)
@@ -20,12 +9,3 @@ class Tests(TestCase):
     def test_redirect(self):
         response = self.client.get('/redirect/')
         self.assertEqual(response.status_code, 302)
-
-    def test_user_data_presence(self):
-        user_count = models.User.objects.filter(name='Первый', email='pervi@gmail.com', phone='88888888888', ).count()
-        self.assertEqual(user_count, 1)
-
-    def test_post_list_view(self):
-        response = self.client.get('/post/')
-        self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'core/post_list.html')
