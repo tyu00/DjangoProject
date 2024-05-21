56965c0efac566f7070ab0cd245ef8133028945d
tyu00 <g.s.u.toshov2002@yandex.com>
2024-05-21 16:05:11 +0500

    10

--- core/models.py
+++ core/models.py
@@ -10,6 +10,7 @@ class User(models.Model):
     avatar = models.ImageField('автарка', upload_to='avatars', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_rare = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = 'Пользователь'
@@ -26,6 +27,7 @@ class Post(models.Model):
     image = models.ImageField(upload_to='posts', null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    is_rare = models.BooleanField(default=False)
 
     class Meta:
         verbose_name = 'Пост'
